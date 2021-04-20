9f4978ab29e158580ad2ec0fb02035e9549b4605
Priyaranjan-Jha <jha.priyaranjan30@gmail.com>
2021-04-20 20:56:21 +0530

    added inheritance relationship between Ticket class (Parent) & RegularTicket, TouristTicket (child) in FRS project

--- FRS_Java/src/com/com/upgrad/frs/Main.java
+++ FRS_Java/src/com/com/upgrad/frs/Main.java
@@ -5,14 +5,15 @@ public class Main {
     public static void main(String[] args) {
       Flight flight =new Flight("A740" , "IndiGo", 500,400);
       Passenger passenger=new Passenger("Chetla","kolkata","West Bengal","Priya","1234567890","priya30@gmail.com");
-     RegularTicket regularTicket=new RegularTicket("A140","Delhi","Chennai","12/10/20 10:50","12/10/20 12:55","F17",5000,false,"food available",flight,passenger);
+      RegularTicket rt = new RegularTicket("A123","kolkata","Chennai",flight,"12/03/21 08:20","12/03/21 11:20",passenger,"A09",5000,false,"Food and spa included");
 
-
+      System.out.println(rt.getDepartureDateTime());
+      System.out.println(rt.getSpecialServices());
       System.out.println(flight.getAirLine());
       System.out.println(flight.getBookedSeats());
       System.out.println(flight.getCapacity());
 
-      System.out.println(regularTicket.getFrom());
+
 
         }
 
--- FRS_Java/src/com/com/upgrad/frs/TouristTicket.java
+++ FRS_Java/src/com/com/upgrad/frs/TouristTicket.java
@@ -1,62 +1,20 @@
 package com.upgrad.frs;
 
-public class TouristTicket {
+public class TouristTicket extends Ticket {
     //make all attributes private
-    private String pnr;
-    private String from;
-    private String to;
-    private String departureDateTime;
-    private String arrivalDateTime;
-    private String seatNo;
-    private float price;
-    private boolean cancelled;
+
     private String hotelAddress;
     private String[] selectedTouristLocation;
-    private Flight flight;
-    private Passenger passenger;
 
-    public TouristTicket(String pnr, String from, String to, String departureDateTime, String arrivalDateTime, String seatNo, float price, boolean cancelled, String hotelAddress,Flight flight, Passenger passenger) {
-        this.pnr = pnr;
-        this.from = from;
-        this.to = to;
-        this.departureDateTime = departureDateTime;
-        this.arrivalDateTime = arrivalDateTime;
-        this.seatNo = seatNo;
-        this.price = price;
-        this.cancelled = cancelled;
+    // Using super() keyword to call constructor from Ticket class
+
+    public TouristTicket(String pnr, String from, String to, Flight flight,String departureDateTime, String arrivalDateTime,Passenger passenger, String seatNo, float price, boolean cancelled,String hotelAddress,String[] selectedTouristLocation) {
+        super(pnr, from, to, flight, departureDateTime, arrivalDateTime, passenger, seatNo, price, cancelled);
         this.hotelAddress = hotelAddress;
         this.selectedTouristLocation = new String[5];
-        this.flight=flight;
-        this.passenger=passenger;
-    }
-
-    TouristTicket(TouristTicket touristTicket) {
-        this.pnr = touristTicket.pnr;
-        this.from = touristTicket.from;
-        this.to = touristTicket.to;
-        this.departureDateTime = touristTicket.departureDateTime;
-        this.arrivalDateTime = touristTicket.arrivalDateTime;
-        this.seatNo = touristTicket.seatNo;
-        this.price = touristTicket.price;
-        this.cancelled = touristTicket.cancelled;
-        this.hotelAddress = touristTicket.hotelAddress;
-        this.selectedTouristLocation = touristTicket.selectedTouristLocation;
-        this.flight= touristTicket.flight;
-        this.passenger= touristTicket.passenger;
 
     }
 
-    String checkStatus() {
-        if (cancelled == true) {
-            return "Cancelled";
-        } else {
-            return "Confirmed";
-        }
-    }
-
-    int getFlightDuration() {
-        return Integer.parseInt(arrivalDateTime) - Integer.parseInt(departureDateTime);
-    }
 
 
     String getHotelAddress() {
@@ -65,71 +23,6 @@ public class TouristTicket {
 
     String[] getTouristLocation() {
         return this.selectedTouristLocation;
-    }
-
-    // getters and setters for private attributes
-    public String getPnr() {
-        return pnr;
-    }
-
-    public void setPnr(String pnr) {
-        this.pnr = pnr;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public String getDepartureDateTime() {
-        return departureDateTime;
-    }
-
-    public void setDepartureDateTime(String departureDateTime) {
-        this.departureDateTime = departureDateTime;
-    }
-
-    public String getArrivalDateTime() {
-        return arrivalDateTime;
-    }
-
-    public void setArrivalDateTime(String arrivalDateTime) {
-        this.arrivalDateTime = arrivalDateTime;
-    }
-
-    public String getSeatNo() {
-        return seatNo;
-    }
-
-    public void setSeatNo(String seatNo) {
-        this.seatNo = seatNo;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    public void setCancelled(boolean cancelled) {
-        this.cancelled = cancelled;
     }
 
     public void setHotelAddress(String hotelAddress) {
@@ -144,21 +37,7 @@ public class TouristTicket {
         this.selectedTouristLocation = selectedTouristLocation;
     }
 
-    public Flight getFlight() {
-        return flight;
-    }
 
-    public void setFlight(Flight flight) {
-        this.flight = flight;
-    }
-
-    public Passenger getPassenger() {
-        return passenger;
-    }
-
-    public void setPassenger(Passenger passenger) {
-        this.passenger = passenger;
-    }
 }
 
 
